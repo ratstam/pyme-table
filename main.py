5a58b37865094a790954569130c9c7173f5d7022
MatStar15 <39085851+MatStar15@users.noreply.github.com>
2020-11-21 17:49:18 +0100

    started the project
    created flowchart

---
+++ main.py
@@ -0,0 +1,31 @@
+import openpyxl as xl
+
+classes_wb = xl.load_workbook('classes.xlsx')
+classes_sheet = classes_wb['Sheet1']
+
+cell = classes_sheet.cell(2,1)
+
+class_hours = {
+	class_name : class_hours ,
+	'1h' : '' ,
+	'2h' : '' ,
+	'3h' : '' ,
+	'4h' : '' ,
+	'5h' : '' ,
+	'6h' : '' ,
+}
+
+class_names = 1
+classes = ''
+
+
+for row in range(2, classes_sheet.max_row + 1):
+	class_name = classes_sheet.cell(row, class_names).value
+	classes += class_name + ", "
+print(classes)
+
+total_lessons_amount = 5
+
+for row in range(2, classes_sheet.max_row + 1):
+	lessons_amount = classes_sheet.cell(row, total_lessons_amount).
+	
