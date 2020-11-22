3a73ac8cc9f1a50b38e9d204889e9f5c3f2c4d25
MatStar15 <39085851+MatStar15@users.noreply.github.com>
2020-11-22 23:40:17 +0100

    Major advancemts

--- main.py
+++ main.py
@@ -1,90 +1,55 @@
 import openpyxl as xl
 import random
-# from array import *   # unused
+
 
 classes_wb = xl.load_workbook('classes.xlsx')
 classes_sheet = classes_wb['Sheet1']
 cell = classes_sheet.cell(2,1)
 
 
-class_name = ''
-class_hours = 0
-class_names = 1
-classes = ''
-days = ("Monday", "Tuesday", "Wednesday", "Thusday", "Fridat", "Saturday", "Sunday")
+# unused
+	# class_name = '' 
+	# class_hours = 0
+	# class_names = 1
+	# classes = ''
+
+days = ("Monday", "Tuesday", "Wednesday", "Thusday", "Friday", "Saturday", "Sunday")
 hours = (1, 2, 3, 4, 5)
 worksheet_to_array = []
 temp_row_worksheet_to_array = []
 time_table = []
-professor = ''
 professor_hours = 0
-lessons = []
-#  Debug
-# print('Debug 1')
-
+class_lessons = []
+class_lessons_array = []
 loop_rows = classes_sheet.max_row + 1
 loop_columns = classes_sheet.max_column + 1
 
 
 for row in range(2, loop_rows):
-	# print('Debug 2')
-	
 	for column in range(1, loop_columns):
-		#  Debug
-		# print(f'{column}, {row}')
-		# print('Debug 3')
-		# print(classes_sheet.cell(row, column).value)
-
 		cell_value = classes_sheet.cell(row, column).value
-
-		# Debug
-		# print(f" row: {row}, column: {column}")
-
-
 		temp_row_worksheet_to_array.append(cell_value)
 	worksheet_to_array.append(temp_row_worksheet_to_array)
+	temp_row_worksheet_to_array = []
 
-
-#  Debug
-# print(temp_worksheet_to_array)
-# print(worksheet_to_array)
 for row in worksheet_to_array:
 	row_lenght = len(row)
 	row_number = worksheet_to_array.index(row)
-	for cell_index_p1 in range(2, row_lenght):
+	for cell_index_p1 in range(2, row_lenght + 1):
 		cell_index = cell_index_p1 - 1
-		# cell_index = worksheet_to_array[row_number].index(cell)
 		cell = row[cell_index]
-		print(cell_index)
-
-
 		if cell_index != 0:
-			if float(cell_index % 2) == 0:
-				a = 1
-				# professor = cell
-				# print(f"{cell} is a professor")
+			loop_statement = cell_index % 2
+			if loop_statement == 0:
+				professor_hours += int(cell)
 			else:
-				a = 1
-				# print(f"{cell} is the amount of hours")
-				# professor_hours += int(cell)
+				professor = cell
 			for hour in range(1, professor_hours+1):
-				lessons.append(professor)
-		elif cell_index == 0:
-			print(cell)
-
-
+				class_lessons.append(professor)
+			professor_hours = 0
+	class_lessons_array.append(class_lessons)
+	class_lessons = []
 
 
 # Debug
-# print(lessons)
-
-
-# for row in range(2, classes_sheet.max_row + 1):
-# 	class_name = classes_sheet.cell(row, class_names).value
-# 	classes += class_name + ", "
-# print(classes)
-
-# total_lessons_amount = 5
-
-# for row in range(2, classes_sheet.max_row + 1):
-# 	lessons_amount = classes_sheet.cell(row, total_lessons_amount)
+# print(class_lessons_array)
